140fdffd86c3b9647b416dbbbb78fe891dec7a57
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-09-19 11:20:36 +0100

    ameliorer la performance de apk

--- src/Main.java
+++ src/Main.java
@@ -54,58 +54,70 @@ public class Main {
                     if(authService.login(loginEmail,loginPassword)){
                         int choix2;
                         do {
-                            System.out.println("1. Show profile");
-                            System.out.println("2. Change password");
-                            System.out.println("3. Change email");
-                            System.out.println("4. Change address");
-                            System.out.println("5. Create account");
-                            System.out.println("6. list my accounts");
-                            System.out.println("7. Balance of account(By id of account)");
-                            System.out.println("8. Close account(By id of account)");
-                            System.out.println("9. Deposit");
-                            System.out.println("10. Withdraw");
-                            System.out.println("11. Transfer");
-                            System.out.println("12. Historique");
-                            System.out.println("13. logout");
+                            System.out.println("1. Profile User");
+                            System.out.println("2. Create account");
+                            System.out.println("3. list my accounts");
+                            System.out.println("4. Balance of account(By id of account)");
+                            System.out.println("5. Close account(By id of account)");
+                            System.out.println("6. Deposit");
+                            System.out.println("7. Withdraw");
+                            System.out.println("8. Transfer");
+                            System.out.println("9. Historiques");
+                            System.out.println("10. logout");
                             choix2 = reader.nextInt();
                             switch (choix2){
                                 case 1:
-                                    authService.showProfile();
+                                    int choix3;
+                                    do {
+                                        System.out.println("1. Show profile");
+                                        System.out.println("2. Change password");
+                                        System.out.println("3. Change email");
+                                        System.out.println("4. Change address");
+                                        System.out.println("0. Exit");
+                                        choix3 = reader.nextInt();
+                                        switch (choix3){
+                                            case 1:
+                                                authService.showProfile();
+                                                break;
+                                            case 2:
+                                                System.out.println("Enter old password : ");
+                                                String oldPassword = reader.next();
+                                                System.out.println("Enter new password : ");
+                                                String newPassword = reader.next();
+                                                authService.changePassword(oldPassword,newPassword);
+                                                break;
+                                            case 3:
+                                                System.out.println("Enter new email : ");
+                                                String newEmail = reader.next();
+                                                authService.updateEmail(newEmail);
+                                                break;
+                                            case 4:
+                                                System.out.println("Enter new address : ");
+                                                String newAddress = reader.next();
+                                                authService.updateAddress(newAddress);
+                                                break;
+                                            default:
+                                                System.out.println("Wrong choix");
+                                        }
+                                    }while (choix3 != 0);
                                     break;
                                 case 2:
-                                    System.out.println("Enter old password : ");
-                                    String oldPassword = reader.next();
-                                    System.out.println("Enter new password : ");
-                                    String newPassword = reader.next();
-                                    authService.changePassword(oldPassword,newPassword);
-                                    break;
-                                case 3:
-                                    System.out.println("Enter new email : ");
-                                    String newEmail = reader.next();
-                                    authService.updateEmail(newEmail);
-                                    break;
-                                case 4:
-                                    System.out.println("Enter new address : ");
-                                    String newAddress = reader.next();
-                                    authService.updateAddress(newAddress);
-                                    break;
-                                case 5:
                                     accountService.createAccount();
                                     break;
-                                case 6:
+                                case 3:
                                     accountService.listUserAccounts();
                                     break;
-                                case 7:
+                                case 4:
                                     System.out.println("Please enter your account number");
                                     String accountNumber = reader.next().trim();
                                     accountService.getBalance(accountNumber);
                                     break;
-                                case 8:
+                                case 5:
                                     System.out.println("Please enter your account number : ");
                                     String accountClose =  reader.next().trim();
                                     accountService.closeAccount(accountClose);
                                     break;
-                                case 9:
+                                case 6:
                                     System.out.println("Please enter your account number : ");
                                     String accountDeposit = reader.next().trim();
                                     System.out.println("Please enter the amount : ");
@@ -115,7 +127,7 @@ public class Main {
                                     String depositDescription = reader.nextLine().trim();
                                     transactionService.deposit(accountDeposit,depositAmount,depositDescription);
                                     break;
-                                case 10:
+                                case 7:
                                     System.out.print("Please enter your account number : ");
                                     String accountWithdraw = reader.next().trim();
                                     System.out.print("Please enter the amount : ");
@@ -125,7 +137,7 @@ public class Main {
                                     String withdrawDescription = reader.nextLine().trim();
                                     transactionService.withdraw(accountWithdraw,withdrawAmount,withdrawDescription);
                                     break;
-                                case 11:
+                                case 8:
                                     System.out.println("Please enter the from account number :");
                                     String accountFrom = reader.next().trim();
                                     System.out.println("Please enter the to account number :");
@@ -137,17 +149,17 @@ public class Main {
                                     String accountBalanceDescription = reader.nextLine().trim();
                                     transactionService.transfer(accountFrom,accountTo,accountBalance,accountBalanceDescription);
                                     break;
-                                case 12:
+                                case 9:
                                     System.out.println("Please enter your account number :");
                                     String accountHistory = reader.next().trim();
                                     transactionService.history(accountHistory);
-                                case 13:
+                                case 10:
                                     authService.logout();
                                     break;
                                 default:
                                     System.out.println("Wrong choice");
                             }
-                        }while(choix2 != 13);
+                        }while(choix2 != 10);
                     }
                     break;
                 case 0:
